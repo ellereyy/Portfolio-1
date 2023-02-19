084c23f99dbb3d7db8bfea930dedd742f4cd7097
username <elle.m.reynolds@gmail.com>
2023-02-18 22:38:17 -0800

    added transitions to home and about pages

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 // PLAY FLIGHT VIDEO
 const homeImg = document.getElementById("fly-pic");
 const homeVid = document.getElementById("fly-vid");
+const homePage = document.getElementById('home-page');
+const homeBtn = document.getElementById('home-btn');
+const abtBtn = document.getElementById('abt-btn');
+const test = document.getElementById('test');
 
+test.addEventListener('click', () => {
+    homePage.classList.add('slide-out');
+});
 
 homeImg.addEventListener("mouseover", () => {
     homeImg.style.display = "none";
     homeVid.play();
 });
-
 
 homeVid.addEventListener('ended', () => {
     homeImg.style.display = 'block';
