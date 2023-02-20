c69f6b2ac19e73de2a39cb19abec3cd73197c705
username <elle.m.reynolds@gmail.com>
2023-02-19 21:47:07 -0800

    fixed video function

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 // PAGE CHANGE ANIMATIONS 
-//IDEAS 
-    //define the function separately vs definining it as it's being passed 
-    //change the links to #s and update JS functions to open the page, 
-        //include animation to every CSS body, but only add exit styling via JS 
-        //make sure exit styling is removed after the click 
+// IDEAS 
+//     define the function separately vs definining it as it's being passed 
+//     change the links to #s and update JS functions to open the page, 
+//         include animation to every CSS body, but only add exit styling via JS 
+//         make sure exit styling is removed after the click 
 
 
-// const homePage = document.getElementById('home-page');
+// const test = document.getElementById('test');
+
+// const homePage = document.getElementById('hp');
 // const abtPage = document.getElementById('about-page');
+// console.log(abtPage)
+// console.log(homePage)
 
 // const homeBtn = document.getElementById('home-btn');
 // const abtBtn = document.getElementById('abt-btn');
-// const test = document.getElementById('test');
 
 // abtBtn.addEventListener('click', () => {
 //     homePage.classList.add('slide-out');
-//     abtPage.classList.add('slide-in');
-// });
+//     setTimeout(() => {
+//         window.location.href = 'about-page.html';
+//     }, 4000);
+// })
 
 
 // PLAY FLIGHT VIDEO  
-const homeImg = document.getElementById("fly-pic");
-const homeVid = document.getElementById("fly-vid");
+const flyVid = document.getElementById('fly-vid')
 
-homeImg.addEventListener("click", () => {
-    homeImg.style.display = "none";
-    homeVid.play();
-});
+flyVid.addEventListener('click', () => {
+    flyVid.play();
+})
 
-homeVid.addEventListener('ended', () => {
-    homeImg.style.display = 'block';
-});
+flyVid.addEventListener('ended', () => {
+    flyVid.load();
+})
 
 //DISPLAY/HIDE MODALS 
-const openBtn = document.getElementById('openModal');
+const openBtn = document.getElementById('openModal')
 const modal = document.getElementById('modal')
 const closeBtn = document.getElementById('close')
 
@@ -44,11 +47,5 @@ const closeModal = () => {
   modal.style.display = 'none';
 }
 
-openBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-
-
-
-
-
-
+openBtn.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
--- ideas.md
+++ ideas.md
@@ -1,3 +1,56 @@
 js idea - still photo for main photobackground that turns into a video during a click 
 
 include a modal for image references 
+
+
+dry code - use an index for each btn that is pressed 
+
+current page to new page index 
+
+
+
+JS GRAVEYARD
+
+// const homeImg = document.getElementById("fly-pic");
+// const homeVid = document.getElementById("fly-vid");
+
+// // homeImg.addEventListener("click", () => {
+// //     homeImg.style.display = "none";
+// //     homeVid.play();
+// // });
+
+// // homeVid.addEventListener('ended', () => {
+// //     homeImg.style.display = 'block';
+// // });
+
+
+CSS GRAVEYARD
+
+/* LAYOUT ATTEMPT 1 FOR IMAGE/VIDEO */
+/* .img-vid-container {
+    position: relative;
+    height: 500px;
+}
+
+.fly-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    overflow: hidden;
+}
+
+.fly {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+  
+.fly:active {
+    display: block;
+}
+
+#fly-pic {
+    animation: fade 1s ease-in both;
+} */
