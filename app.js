254b66269e07b8af8cb045dec601a94cf10070a1
username <elle.m.reynolds@gmail.com>
2023-02-19 18:54:04 -0800

    added first modal

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-// PLAY FLIGHT VIDEO
+// PAGE CHANGE ANIMATIONS 
+//IDEAS 
+    //define the function separately vs definining it as it's being passed 
+    //change the links to #s and update JS functions to open the page, 
+        //include animation to every CSS body, but only add exit styling via JS 
+        //make sure exit styling is removed after the click 
+
+
+// const homePage = document.getElementById('home-page');
+// const abtPage = document.getElementById('about-page');
+
+// const homeBtn = document.getElementById('home-btn');
+// const abtBtn = document.getElementById('abt-btn');
+// const test = document.getElementById('test');
+
+// abtBtn.addEventListener('click', () => {
+//     homePage.classList.add('slide-out');
+//     abtPage.classList.add('slide-in');
+// });
+
+
+// PLAY FLIGHT VIDEO  
 const homeImg = document.getElementById("fly-pic");
 const homeVid = document.getElementById("fly-vid");
-const homePage = document.getElementById('home-page');
-const homeBtn = document.getElementById('home-btn');
-const abtBtn = document.getElementById('abt-btn');
-const test = document.getElementById('test');
 
-test.addEventListener('click', () => {
-    homePage.classList.add('slide-out');
-});
-
-homeImg.addEventListener("mouseover", () => {
+homeImg.addEventListener("click", () => {
     homeImg.style.display = "none";
     homeVid.play();
 });
@@ -18,3 +31,24 @@ homeImg.addEventListener("mouseover", () => {
 homeVid.addEventListener('ended', () => {
     homeImg.style.display = 'block';
 });
+
+//DISPLAY/HIDE MODALS 
+const openBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('close')
+
+const openModal = () => {
+  modal.style.display = 'block';
+}
+const closeModal = () => {
+  modal.style.display = 'none';
+}
+
+openBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+
+
+
+
+
+
