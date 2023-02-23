24f32fa1f4681fb231c031d39204550fafb24a04
username <elle.m.reynolds@gmail.com>
2023-02-22 19:57:48 -0800

    responsive design complete

--- JS/app2.js
+++ JS/app2.js
@@ -1,9 +1,36 @@
+//HAMBURGER MENU
+
+const hamburger = document.querySelector('.hamburger');
+
+const navLinks = document.querySelector('.nav-links');
+const logo = document.querySelector('#logo');
+
+hamburger.addEventListener('click', () => {
+  logo.classList.add('hide');
+  hamburger.classList.add('hide');
+  navLinks.classList.toggle('show');
+});
+
+document.addEventListener('click', (event) => {
+  if (navLinks.classList.contains('show') && !event.target.classList.contains('nav-links') && !event.target.classList.contains('hamburger') && !event.target.classList.contains('nav-btn')) {
+    navLinks.classList.remove('show');
+  }
+});
+
+let navBtns = document.getElementsByClassName('nav-btn');
+
+navBtns.addEventListener('click', () =>{
+  navLinks.classList.add('hide');
+});
+
+
 
 
 //SLIDE OUT ON PAGE EXIT
-let navBtns = document.getElementsByClassName('nav-btn');
-let mainContent = document.querySelector('.box-layout');
 
+let mainContent = document.querySelector('.box-layout');
+// let navBtns = document.getElementsByClassName('nav-btn');
+// variable commented out because it's listed above, keeping it here for my own mental sanity on the next function
 
 for (let i = 0; i < navBtns.length; i++) {
   navBtns[i].addEventListener('click', (event) => {
@@ -12,11 +39,10 @@ for (let i = 0; i < navBtns.length; i++) {
       mainContent.classList.add('fade-out');
       setTimeout(() => {
         window.location.href = event.target.href;
-      }, 1000);
+      }, 800);
     }
   });
 };
-
 
 
 //DISPLAY/HIDE MODALS - DONE, just need to clean up CSS + js display setting maybe  
@@ -34,9 +60,11 @@ const closeModal = () => {
 openBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 
+
 // PLAY FLIGHT VIDEO: DONE
 const flyVid = document.getElementById('fly-vid')
 
+if (window.location.href.includes("about.html")) {
   flyVid.addEventListener('mouseover', () => {
       flyVid.play();
   });
@@ -46,3 +74,4 @@ const flyVid = document.getElementById('fly-vid')
   flyVid.addEventListener('ended', () => {
       flyVid.load();
   });
+}
